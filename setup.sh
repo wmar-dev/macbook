d5c886ccc6eb8192cffab712b21386274008644c
Warren Mar <241484506+wmar-dev@users.noreply.github.com>
2026-01-08 13:36:51 -0800

    Add graphviz to brew install.

--- setup.sh
+++ setup.sh
@@ -119,9 +119,10 @@ print_header "Installing Code Editors and IDEs"
 print_header "Installing Productivity Tools"
 
 tools=(
-    gh
-    ffmpeg
     deno
+    ffmpeg
+    gh
+    graphviz
 )
 
 for tool in "${tools[@]}"; do
